8a1d03b3a759b2dd442ce64fc172f4e8173bc02a
PanagiotisVard <cs04590@uoi.gr>
2023-09-18 03:11:00 +0300

    Order Page with static data

--- frontend/src/Pages/OrderPage/OrderPage.js
+++ frontend/src/Pages/OrderPage/OrderPage.js
@@ -2,13 +2,32 @@ import React from 'react';
 import Header from "../Header/Header";
 import Footer from '../Footer/Footer';
 import { Container, Typography, Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const centerTextStyles = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  minHeight: '100px', // You can adjust the height as needed
+  minHeight: '100px',
 };
+
+const buttonsContainerStyles = {
+  display: 'flex',
+  alignItems: 'center',
+  marginTop: '16px',
+};
+
+const sendOrderButtonStyles = {
+  flex: 2, // Takes 2 times the space compared to the "BACK" button
+  marginRight: '8px', // Adjust the spacing between buttons
+};
+
+const backButtonStyles = {
+  flex: 1,
+  marginLeft: '8px',
+  marginTop: '20px', // Adjust the marginTop as needed
+};
+
 
 function Order() {
   // Sample data for dishes
@@ -53,14 +72,27 @@ function Order() {
             </TableBody>
           </Table>
         </TableContainer>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSendOrder}
-          fullWidth
-        >
-          SEND ORDER
-        </Button>
+        <div style={buttonsContainerStyles}>
+          {/* SEND ORDER button */}
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSendOrder}
+            style={sendOrderButtonStyles}
+          >
+            SEND ORDER
+          </Button>
+          {/* BACK button */}
+          <Button
+            component={Link}
+            to="/"
+            variant="contained"
+            className="back-button"
+            style={backButtonStyles}
+          >
+            BACK
+          </Button>
+        </div>
       </Container>
       <Footer />
     </div>
